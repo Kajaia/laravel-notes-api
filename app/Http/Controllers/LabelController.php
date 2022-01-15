d7e5eb6afe8d680f6a83d9a7cc4ef558626fd7d6
Kajaia <lashakajaia31@gmail.com>
2022-01-16 01:51:59 +0400

    Added labels api

---
+++ app/Http/Controllers/LabelController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\LabelResource;
+use App\Models\Label;
+use Illuminate\Http\Request;
+
+class LabelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return LabelResource::collection(Label::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $label = Label::create([
+            'title' => $request->title
+        ]);
+
+        return new LabelResource($label);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $label = Label::findorfail($id);
+
+        return new LabelResource($label);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $label = Label::findorfail($id);
+
+        $label->update([
+            'title' => $request->title
+        ]);
+
+        return new LabelResource($label);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $label = Label::findorfail($id);
+
+        $label->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ app/Http/Resources/LabelResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LabelResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'created_at' => (string) $this->created_at,
+            'updated_at' => (string) $this->updated_at
+        ];
+    }
+}
