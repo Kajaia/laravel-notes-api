6bc276df37c9ccd489af7e2f535f42101c7128d1
Kajaia <lashakajaia31@gmail.com>
2022-01-16 13:39:45 +0400

    Updated welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,54 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#ADD8E6">
+    <link rel="shortcut icon" href="https://ssl.gstatic.com/keep/keep_2020q4v2.ico" type="image/x-icon">
     <title>Google Keep Clone</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0 auto;
+            color: #fff;
+            background-color: #ADD8E6;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        .content {
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+        }
+
+        .mx-1 {
+            margin-left: 10px;
+            margin-right: 10px;
+        }
+
+        .mn-1 {
+            margin-top: -10px;
+        }
+
+        @media only screen and (max-width: 425px) {
+            .content {
+                flex-direction: column;
+            }
+        }
+    </style>
 </head>
 <body>
-    <h1>Google Keep Clone - Under Construction!</h1>
+    <div class="content">
+        <div class="mx-1">
+            <img height="100" src="https://seeklogo.com/images/G/google-keep-logo-0BC92EBBBD-seeklogo.com.png" alt="img">
+        </div>
+        <div class="mx-1">
+            <h1>Google Keep Clone</h1>
+            <p class="mn-1">Under Construction!</p>
+        </div>
+    </div>
 </body>
 </html>
