7df3db0c20527c9f9446130492a4c36315952617
Kajaia <lashakajaia31@gmail.com>
2022-01-16 16:19:08 +0400

    Installed react and added example component in welcome view

---
+++ resources/js/components/Example.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Example() {
+    return (
+        <div class="d-flex align-items-center justify-content-center vh-100">
+            <div class="row justify-content-center">
+                <div class="col-md-12 my-auto">
+                    <h1 class="mb-0">Google Keep Clone</h1>
+                    <p>Under Construction!</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Example;
+
+if (document.getElementById("example")) {
+    ReactDOM.render(<Example />, document.getElementById("example"));
+}
